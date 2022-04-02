bdd11a550c8895decb560a71a5e66d7455e0fec0
Jimmie Leslee <jleslee@ev-p6.kzn.21-school.ru>
2022-04-02 13:26:06 +0300

    second commit

--- ft_printf.c
+++ ft_printf.c
@@ -7,13 +7,13 @@ void	ft_putnbr(long dig, int len, char *sign) {
 	write (1, &sign[dig % len], 1);
 	g_var++;
 }
-int	ft_printf(const char *format, ...) {
+int	ft_printf(const char *fmt, ...) {
 	va_list	ap;
-	va_start(ap, format);
-	while (*format) {
-		if (*format == '%') {
-			format++;
-			if (*format == 's') {
+	va_start(ap, fmt);
+	while (*fmt) {
+		if (*fmt == '%') {
+			fmt++;
+			if (*fmt == 's') {
 				int	len = 0;
 				char *str = va_arg(ap, char *);
 				if (!str)
@@ -23,7 +23,7 @@ int	ft_printf(const char *format, ...) {
 				g_var += len;
 				write(1, str, len);
 			}
-			else if (*format == 'd') {
+			else if (*fmt == 'd') {
 				int d = va_arg(ap, int);
 				if (d < 0) {
 					write(1, "-", 1);
@@ -32,14 +32,14 @@ int	ft_printf(const char *format, ...) {
 				}
 				ft_putnbr(d, 10, "0123456789");
 			}
-			else if (*format == 'x') {
+			else if (*fmt == 'x') {
 				int x = va_arg(ap, unsigned);
 				ft_putnbr(x, 16, "0123456789abcdef");
 			}
-			format++;
+			fmt++;
 		}
 		else
-			g_var += write(1, format++, 1);
+			g_var += write(1, fmt++, 1);
 	}
 	va_end(ap);
 	return(g_var);
--- ft_printf_with_comments.c
+++ ft_printf_with_comments.c
@@ -7,13 +7,13 @@ void	ft_putnbr(long dig, int len, char *sign) { //len: для %d == 10 для %x
 	write (1, &sign[dig % len], 1); //выводим по символьно нашы цифры
 	g_var++; // увеличивая кол-во напечатанных символов
 }
-int	ft_printf(const char *format, ...) {
+int	ft_printf(const char *fmt, ...) {
 	va_list	ap;
-	va_start(ap, format);
-	while (*format) { //идем по строке пока не закончится
-		if (*format == '%') { // если встречаем процент, то...
-			format++; //...смотрим какой спецификатор стоит после него
-			if (*format == 's') { //если спецификатора s
+	va_start(ap, fmt);
+	while (*fmt) { //идем по строке пока не закончится
+		if (*fmt == '%') { // если встречаем процент, то...
+			fmt++; //...смотрим какой спецификатор стоит после него
+			if (*fmt == 's') { //если спецификатора s
 				int	len = 0;
 				char *str = va_arg(ap, char *);
 				if (!str)
@@ -23,7 +23,7 @@ int	ft_printf(const char *format, ...) {
 				g_var += len; //присваиваем подсчитанное кол-во символов к глобальной переменной
 				write(1, str, len); //выводим строку
 			}
-			else if (*format == 'd') { //если спецификатор d
+			else if (*fmt == 'd') { //если спецификатор d
 				int d = va_arg(ap, int);
 				if (d < 0) { //если отрицательный, то...
 					write(1, "-", 1); //выводим минус
@@ -32,14 +32,14 @@ int	ft_printf(const char *format, ...) {
 				}
 				ft_putnbr(d, 10, "0123456789");
 			}
-			else if (*format == 'x') { // если спецификатор х
+			else if (*fmt == 'x') { // если спецификатор х
 				int x = va_arg(ap, unsigned);
 				ft_putnbr(x, 16, "0123456789abcdef"); //отправляем на вывод
 			}
-			format++; //переходим на следующий символ
+			fmt++; //переходим на следующий символ
 		}
 		else // если знака процента нет то просто выводим по символьно текст
-			g_var += write(1, format++, 1);
+			g_var += write(1, fmt++, 1);
 	}
 	va_end(ap);
 	return(g_var);
